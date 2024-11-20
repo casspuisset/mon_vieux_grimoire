5a524e637591d03f5600471c5e19a943a8fae63e
Cassandre Puisset <cpuisset98@gmail.com>
2024-11-20 10:34:33 +0100

    configuration db

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,86 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://user:oXyg0UaVXaaWcEh5@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.post('/api/auth/login', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé'
+    })
+})
+
+app.get('/api/books', (req, res, next) => {
+    const stuff = [
+        {
+            "id": "1",
+            "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
+            "title" : "Milwaukee Mission",
+            "author": "Elder Cooper",
+            "imageUrl" : "https://via.placeholder.com/206x260",
+            "year" : 2021,
+            "genre" : "Policier",
+            "ratings" : [{
+              "userId" : "1",
+              "grade": 5
+            },
+              {
+                "userId" : "1",
+                "grade": 5
+              },
+              {
+                "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
+                "grade": 5
+              },
+              {
+                "userId" : "1",
+                "grade": 5
+              }],
+            "averageRating": 3
+          },
+            {
+              "id": "2",
+              "userId" : "clbxs3tag6jkr0biul4trzbrv",
+              "title" : "Book for Esther",
+              "author": "Alabaster",
+              "imageUrl" : "https://via.placeholder.com/206x260",
+              "year" : 2022,
+              "genre" : "Paysage",
+              "ratings" : [{
+                "userId" : "clbxs3tag6jkr0biul4trzbrv",
+                "grade": 4
+              },
+                {
+                  "userId" : "1",
+                  "grade": 5
+                },
+                {
+                  "userId" : "1",
+                  "grade": 5
+                },
+                {
+                  "userId" : "1",
+                  "grade": 5
+                }],
+              "averageRating": 4.2
+            },
+    ];
+    res.status(200).json(stuff);
+  });
 
 
 module.exports = app;
